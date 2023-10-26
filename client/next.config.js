786465aa44cf90935fd16174471933e114b7efeb
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-10-26 19:53:30 -0400

    Remove unecessary dependencies

--- client/next.config.js
+++ client/next.config.js
@@ -1,27 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const monacoRules = [
-  {
-    test: /\.ttf$/,
-    type: 'asset/resource'
-  }
-]
-
 const nextConfig = {
     reactStrictMode: false,
 
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-// Modify config and then return it
-if(!isServer) {
-    config.plugins.push(new MonacoWebpackPlugin({
-      languages: ["javascript", "markdown", "yaml"],
-      filename: "static/[name].worker.js",
-    }))
-    config.module.rules.push(...monacoRules)
-  }
-
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
         return config
