f296296dfa7c530977cc2c3c16df74c271476a6e
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2023-10-26 21:17:40 -0400

    Merged

---
+++ client/src/app/homepage.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+
+export default function Homepage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <div className="z-10 w-full items-center justify-center font-mono text-sm lg:flex">
+        <div className="bg-white my-10 p-10 flex flex-col justify-center gap-10">
+          <h1 className="text-2xl">Start using TurboTeX</h1>
+          <button
+            type="button"
+            onClick={() => signIn('descope', { callbackUrl: '/dashboard' })}
+            className="text-[#e9e9e9] bg-[#262d3b] py-2 px-7 border-[#45546e] border-4"
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,14 @@
-import Editor from '../components/editor/Editor';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import Homepage from './homepage';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+  if (session) {
+    redirect('/dashboard');
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-        <Editor />
-      </div>
-    </main>
+    <Homepage />
   );
 }
